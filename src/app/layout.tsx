d300032697285b1713ecf4881687e0120932beb4
Hoang Trang Anh Pham <anhphamhoangtrang@gmail.com>
2025-12-15 01:48:25 +1100

    create and config custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,25 @@
 import { GeistSans } from "geist/font/sans";
+import localFont from "next/font/local";
 import type { Metadata } from "next";
 
 import "~/styles/globals.css";
 import { TRPCReactProvider } from "~/trpc/react";
 
+const myFont = localFont({
+  src: [
+    {
+      path: "../../public/font/DKStickyToffee.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-my-font",
+});
+
 export const metadata: Metadata = {
   title: "BanhMiBandit - Stop chasing opportunities. Let them find you.",
-  description: "Connect your GitHub profile with companies you're targeting. Our AI analyzes your actual code contributions and automatically surfaces the roles where you'd make the biggest impact.",
+  description:
+    "Connect your GitHub profile with companies you're targeting. Our AI analyzes your actual code contributions and automatically surfaces the roles where you'd make the biggest impact.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
@@ -16,13 +29,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} ${myFont.variable}`}>
       <body className="font-sans antialiased">
-        <TRPCReactProvider>
-          {children}
-        </TRPCReactProvider>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
   );
 }
-
